d0a4ba0eed51cc1f0313d8fe0097b8671ea9e236
Umer <umer.salman@swri.org>
2021-09-23 17:23:42 -0500

    Add bootstrap toast for notifs and add usages

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ The recommended way to install composer packages is:
 ```
 composer require swri-sodas/sodas-helper
 ```
+
+TODO:
+elements
+start and end time
+readmore
+SDDAS (generic plotting) - catalog/DB
+plotting (idl/python/sddas/java (autoplot)) - hpca/
+datatables
+spawning a long running process - geoviz/plotting
+bootstrap 5 toasts (notifications)
---
+++ templates/element/bootstrap_toast.php
@@ -0,0 +1,48 @@
+<!--Needs Bootstrap 5-->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<style>
+    .toast {
+        position: sticky;
+        top: 0;
+        float: right;
+        z-index: 999;
+    }
+</style>
+<?php
+
+// USAGE
+//echo $this->Flash->render('flash', [
+//    'element' => 'SodasHelper.bootstrap_toast',
+//    'alertColor' => 'warning',
+//])
+
+/**
+ * @var array $params
+ * @var string $message
+ */
+$class = 'message';
+if (!empty($params['class'])) {
+    $class .= ' ' . $params['class'];
+}
+if (!isset($params['escape']) || $params['escape'] !== false) {
+    $message = h($message);
+}
+
+//primary, secondary, success, danger, warning, info, light, dark
+?>
+
+<div class="toast align-items-center text-white bg-<?= $alertColor ?? 'success' ?> border-0" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+        <div class="toast-body">
+            <?= $message ?>
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+<script>
+    const toast = new bootstrap.Toast(document.getElementsByClassName('toast')[0]);
+    toast.show();
+</script>
