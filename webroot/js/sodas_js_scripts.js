3d95932081d38b0426f7ea644f61b1d13bdf7fa6
Umer <umer.salman@swri.org>
2021-12-21 16:43:38 -0600

    Bootstrap update

--- webroot/js/sodas_js_scripts.js
+++ webroot/js/sodas_js_scripts.js
@@ -20,15 +20,13 @@ function bootstrap_load_or_detect() {
         // load bootstrap
         document.querySelector('head').innerHTML +=
             '<link ' +
-            'href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" ' +
+            'href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" ' +
             'rel="stylesheet" ' +
-            'integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" ' +
             'crossorigin="anonymous">';
 
         const script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js";
-        script.integrity = "sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ";
+        script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js";
         script.crossOrigin = "anonymous";
         document.body.appendChild(script);
         script.addEventListener("load", () => {
