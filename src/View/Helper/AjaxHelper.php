3a5c91ff0994944cebacc1edaacdabe8fdf0bcfc
Umer Salman <umer936@gmail.com>
2022-07-07 01:03:10 -0500

    Add start for AjaxHelper. Working, but needs cleanup. Maybe have it include ajaxFetchWrapper

---
+++ src/View/Helper/AjaxHelper.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+
+namespace SodasHelper\View\Helper;
+
+use Cake\View\Helper;
+
+/**
+ * Ajax helper
+ *
+ * @property \Cake\View\Helper\HtmlHelper $Html
+ * @property \Cake\View\Helper\UrlHelper $Url
+ *
+ * USAGE example:
+ *
+ * echo $this->Ajax->updateOtherDiv(
+ * ['action' => 'showHistory', $subtab1],
+ * 'ShowHistoryBtn',
+ * 'show_history'
+ * );
+ */
+class AjaxHelper extends Helper
+{
+    protected $helpers = ['Html', 'Url'];
+
+    protected $_config;
+
+    /** this function is built in to PHP 8. Needs to be defined for PHP < 8 */
+    private function str_starts_with($haystack, $needle)
+    {
+        return strpos($haystack, $needle) === 0;
+    }
+
+    /**
+     * Just need: Button id (spinner in the button), url, success (id of div to update, and ), error
+     */
+    public function updateOtherDiv($url, $buttonId, $divToUpdate, $postData = null, $divToError = null)
+    {
+        $this->setHeaders();
+
+        if (is_array($url)) {
+            $url = $this->Url->build($url);
+        }
+
+        if (!$this->str_starts_with($buttonId, '#')) {
+            $buttonId = "#$buttonId";
+        }
+
+        if (!$this->str_starts_with($divToUpdate, '#')) {
+            $divToUpdate = "#$divToUpdate";
+        }
+
+        if (!$this->str_starts_with($divToError, '#')) {
+            $divToError = "#$divToError";
+        }
+
+        $fetchOptions = [];
+        if (isset($postData)) {
+            $fetchOptions['body'] = "document.querySelector('$postData')";
+        }
+        $fetchOptions = array_merge_recursive($this->getConfig(), $fetchOptions);
+        $fetchOptions = json_encode($fetchOptions);
+        $script = "
+                function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response
+        }
+    }
+            function parseData(response) {
+        let type = response.headers.get('Content-Type');
+        if (type.includes('json')) {
+            return response.json();
+        } else {
+            return response.text()
+        }
+         }
+         
+         
+//         https://ghinda.net/article/script-tags/
+//         https://stackoverflow.com/questions/1197575/can-scripts-be-inserted-with-innerhtml
+//  https://stackoverflow.com/questions/2592092/executing-script-elements-inserted-with-innerhtml
+         function success(response) {
+           const scriptEl = document.createRange().createContextualFragment(response);
+           const successDiv = document.querySelector('$divToUpdate');
+           successDiv.innerHTML = '';
+         successDiv.append(scriptEl);
+        }
+         ";
+
+        $script .= "
+        document.querySelector('$buttonId').addEventListener('click', function () {
+        fetch(
+            '$url', 
+            $fetchOptions
+        )
+        .then(checkStatus)
+        .then(parseData)
+        .then(success);
+        });
+        ";
+
+//        .catch(settings.error);";
+
+        return $this->Html->scriptBlock($script);
+    }
+
+    private function setHeaders(): void
+    {
+        $this->setConfig('headers.X-CSRF-Token', $this->getView()->getRequest()->getAttribute('csrfToken'));
+        $this->setConfig('headers.X-Requested-With', "XMLHttpRequest");
+    }
+
+
+}
