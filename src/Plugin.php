610d0ad4db41566d67029f407c55f7c9b3ba50a9
Umer Salman <umer936@gmail.com>
2024-01-16 18:17:21 -0600

    Add DebugKit setting

--- src/Plugin.php
+++ src/Plugin.php
@@ -48,6 +48,8 @@ class Plugin extends BasePlugin
                 'function' => "(editor) => {editor.on('init', () => {editor.getContainer().style.transition = 'border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out';});editor.on('focus', () => {editor.getContainer().style.boxShadow = '0 0 0 .2rem rgba(0, 123, 255, .25)';editor.getContainer().style.borderColor = '#80bdff';});editor.on('blur', () => {editor.getContainer().style.boxShadow = '';editor.getContainer().style.borderColor = '';});}"
             ]
         ]);
+
+        Configure::write('DebugKit.safeTld', ['edu', 'org']);
     }
 
     /**
