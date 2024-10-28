8b8985da65a0806107974fae9b73b85b24aa0c15
Umer Salman <umer936@gmail.com>
2024-10-28 18:10:08 -0500

    wrap the bootstrap tooltip enable in a function

--- webroot/js/enable_bootstrap_tooltips.js
+++ webroot/js/enable_bootstrap_tooltips.js
@@ -1,4 +1,8 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+function initializeTooltips() {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+}
+
+initializeTooltips();
